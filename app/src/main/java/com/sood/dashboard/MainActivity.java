a6593eaf3a7cf5c29736eef95fd1b36b1798cf2d
PSchamps <109261738+PSchamps@users.noreply.github.com>
2022-08-17 15:26:23 +0530

    Prabal

--- app/src/main/java/com/sood/dashboard/MainActivity.java
+++ app/src/main/java/com/sood/dashboard/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.view.menu.ShowableListMenu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -14,64 +16,68 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     BottomNavigationView bottomNavigationView;
     ViewFlipper flipper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setSelectedItemId(R.id.menuHome);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch (item.getItemId())
-                {
-                    case R.id.menuHome:
-                        return true;
-                    case R.id.menuAbout:
-                        startActivity(new Intent(getApplicationContext(), About.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.menuAccount:
-                        startActivity(new Intent(getApplicationContext(), Account.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.menuSettings:
-                        startActivity(new Intent(getApplicationContext(), Settings.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-
-                return false;
+            public void onClick(View v) {
+                openAcademies();
             }
         });
-
-        int imgarray[] = {R.drawable.all_sports_banner, R.drawable.images, R.drawable.images__1_, R.drawable.sport_equipment_2_22802518};
-
-        flipper=(ViewFlipper) findViewById(R.id.flipper);
-
-        for(int i = 0;i<imgarray.length;i++)
-        {
-            showimage(imgarray[i]);
-        }
     }
+    public void openAcademies(){
+        Intent intent = new Intent(this, Academies.class);
+        startActivity(intent);
 
-    public void showimage(int img)
-    {
-        ImageView imageview = new ImageView(this);
-        imageview.setBackgroundResource(img);
+        //-------------------------------------------------------------
 
-        flipper.addView(imageview);
-        flipper.setFlipInterval(3000);
-        flipper.setAutoStart(true);
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTournaments();
+            }
+        });
+    }
+    public void openTournaments(){
+        Intent intent = new Intent(this, Tournaments.class);
+        startActivity(intent);
 
-        flipper.setInAnimation(this, android.R.anim.slide_in_left);
-        flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+        //-------------------------------------------------------------
+
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDonate();
+            }
+        });
+    }
+    public void openDonate(){
+        Intent intent = new Intent(this, Donate.class);
+        startActivity(intent);
+
+        //-------------------------------------------------------------
+
+        button = (Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEmpty();
+            }
+        });
+    }
+    public void openEmpty(){
+        Intent intent = new Intent(this, Empty.class);
+        startActivity(intent);
     }
 }
-// hctyfctydtrytt
